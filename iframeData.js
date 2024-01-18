bdb399438e579fde7cc1e64c97b09999ad94c9d0
mbPro <100542453+mbprotheweb@users.noreply.github.com>
2024-01-18 13:06:12 -0500

    Update iframeData.js

--- iframeData.js
+++ iframeData.js
@@ -3,6 +3,6 @@ var yes = new Date();
 yes.setDate(yes.getDate() - 1)
 var iframeArray = [
   { src: "https://spinkablast.wixsite.com/5784/day", milliseconds: 25000 },
-  { src: "https://spinkablast.wixsite.com/5784/day?date=" = yes.toLocaleDateString(), milliseconds: 25000 },
-  { src: "https://spinkablast.wixsite.com/5784/prices" = yes.toLocaleDateString(), milliseconds: 15000 },
+  { src: "https://spinkablast.wixsite.com/5784/day?date=" + yes.toLocaleDateString(), milliseconds: 25000 },
+  { src: "https://spinkablast.wixsite.com/5784/prices", milliseconds: 15000 },
 ];
