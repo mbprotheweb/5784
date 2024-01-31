9ec6395b2d480b1aeaed826dcd0abf1b7d2fc559
mbPro <100542453+mbprotheweb@users.noreply.github.com>
2024-01-31 15:47:07 -0500

    Update iframeData.js

--- iframeData.js
+++ iframeData.js
@@ -6,5 +6,6 @@ var iframeArray = [
   { src: "https://spinkablast.wixsite.com/5784/day?date=" + yes.toLocaleDateString(), milliseconds: 25000 },
   { src: "https://spinkablast.wixsite.com/5784/prices", milliseconds: 15000 },
   { src: "https://spinkablast.wixsite.com/5784/week", milliseconds: 25000 },
+  { src: "https://spinkablast.wixsite.com/5784/week?week=2", milliseconds: 25000 },
   { src: "https://spinkablast.wixsite.com/5784/week?week=1", milliseconds: 25000 }
 ];
